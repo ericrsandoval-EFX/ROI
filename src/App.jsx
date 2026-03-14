aa801ac1936cbec1e06ee61d3694d1de68a4f64b
ericrsandoval-EFX <ericrsandoval@gmail.com>
2026-03-14 16:50:47 -0600

    Create ROI Dashboard component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ROIDashboard = () => {
+    return (
+        <div>
+            <h1>ROI Dashboard</h1>
+            <p>Welcome to the ROI Dashboard!</p>
+            {/* Additional dashboard components and logic can be added here */}
+        </div>
+    );
+};
+
+export default ROIDashboard;
